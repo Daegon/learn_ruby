70a16a27b1be1d2ab2ed15620735010738dd77ee
Daegon <romsssa@gmail.com>
2015-04-17 10:55:41 +0300

    update task 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,16 +1,7 @@
 def translate(sentence)
-  def convert_word(word)
-    if word =~ /^(qu|sch|[^aeiou])/
-      consonant =  word.gsub(/^(qu|sch|[^aeiou]).*$/, '\1')
-      word.gsub!(/^(qu|sch|[^aeiou])(.*)$/, '\2')
-      word = word + consonant
-      word = convert_word word
-    end
-    word
-  end
   sentence = sentence.split
   sentence.map! do |word|
-    word = convert_word(word) + 'ay'
+    word = word.gsub!(/^(qu|sch|[^aeiou]*)(.*)$/, '\2\1') + 'ay'
   end
   sentence.join ' '
 end
